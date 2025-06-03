099792e912c09eee14addc560eb2b59f71b2b6fb
Surafel <surafelmoges01@gmail.com>
2025-06-03 16:34:25 +0100

    Best Progress

--- src/componets/Entry.jsx
+++ src/componets/Entry.jsx
@@ -1,27 +1,30 @@
-export function Entry ( ) {
+export function Entry (props) {
     return (
         <>
          <div className="entry_journal">
             <div className="box">
-                <img src="../images/mount.jpg" className="entry-image"></img>
-                  <img src="../images/mark.jpg" className="marker"
-             alt="Location"  ></img>
+                <img 
+                src={props.img}
+                alt="Mount Fuji"
+                className="entry-image"
+                 />
+                  <img 
+                  src={props.marker}
+                   className="marker"
+                  alt="Location" 
+              />
             <div className="entry-content">
-                
                 <div className="lo">
-                    <p className="country">Japan</p>
- <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" 
-                target="_blank"
-                className="map-link">
-                    <p className="map-link-text">Google Maps Link:</p>
+                    <p className="country">{props.country}</p>
+                   <a href={props.mapLink}
+                      className="map-link">
+                    <p className="map-link-text">{props.mapLinkText}</p>
                 </a>
                 </div>
-                <h1 className="tt">Mount Fuji</h1>
+                <h1 className="tt">{props.title}</h1>
                 <div className="para">
-                    <p className="date-text">12 Jan, 2021 - 24 Jan, 2021</p>
-                   <p className="detail">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). 
-                    Mount Fuji is the single most popular tourist site in Japan,
-                     for both Japanese and foreign touris</p>
+                    <p className="date-text">{props.date}</p>
+                   <p className="detail">{props.detail}</p>
 
                 </div>
                 
